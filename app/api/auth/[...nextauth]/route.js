841c52794dcbb78b88c79649a4ea20a28e9c98be
Shreyash Kumar <kshreyash12345@gmail.com>
2024-07-07 12:11:48 +0530

    test changes2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+import 'dotenv/config';
 import GoogleProvider from 'next-auth/providers/google';
 
 import User from '@models/user';
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-//import Feed from '@components/Feed'
+// import Feed from '@components/Feed'
 import dynamicImport from 'next/dynamic';
 
 export const dynamic = "force-dynamic";
